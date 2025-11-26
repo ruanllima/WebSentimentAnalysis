16c56d0c4375372f4a60612a1446a90e250da4e2
ruanllima <ruanlimafn@gmail.com>
2025-11-26 20:28:30 -0300

    FEAT | Data Prepocessing

--- src/model.py
+++ src/model.py
@@ -13,6 +13,10 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
 import joblib
 
+nltk.download("stopwords")
+# execute 'python -m spacy download en_core_web_sm' in cmd to download the package
+nlp = spacy.load("en_core_web_sm")
+
 # adding dataset
 data = pd.read_excel("./data/twiter_sentiment.xlsx")
 df = pd.DataFrame(data)
@@ -26,11 +30,29 @@ print(df.isna().sum())
 # remove rows with NaN values
 df = df[df["Text"].notna()]
 
-# data balancing chart
-qtdSentiments = []
-labels = df['Sentiment'].unique()
-for i in labels:
-    qtd = df['Sentiment'].value_counts()[i]
-    qtd = int(qtd)
-    qtdSentiments.append(qtd)
-print(qtdSentiments)
+# DEFINE TRAIN AND TEST DATAS
+X_train, X_test, y_train, y_test = train_test_split(df["Text"], df["Sentiment"], random_state=42)
+
+# DATA PREPROCESSING
+
+# Remove numbers, initial characters, and convert to lowercase.
+X_train = [re.sub(r'[^a-zA-Z\s]', '', text).lower() for text in X_train]
+X_test = [re.sub(r'[^a-zA-Z\s]', '', text).lower() for text in X_test]
+
+# Tokenization
+X_train = [phrase.split() for phrase in X_train]
+X_test = [phrase.split() for phrase in X_test]
+
+# Remove Stop Words
+stop_words = set(stopwords.words('english'))
+X_train = [[word for word in phrase if word not in stop_words] for phrase in X_train]
+X_test = [[word for word in phrase if word not in stop_words] for phrase in X_test]
+
+# Lemmatization
+X_train = [' '.join([token.lemma_ for token in nlp(' '.join(phrase))]) for phrase in X_train]
+X_test = [' '.join([token.lemma_ for token in nlp(' '.join(phrase))]) for phrase in X_test]
+
+# TF-IDF Vectorization
+vectorizer = TfidfVectorizer()
+X_train = vectorizer.fit_transform(X_train)
+X_test = vectorizer.transform(X_test)
